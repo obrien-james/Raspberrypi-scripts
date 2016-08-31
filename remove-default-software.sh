cec630cceeedea97f40046fd7d4633c77c016eb7
obrien-james <jameswobrien1987@gmail.com>
2016-08-31 15:38:44 +0100

    Update remove-default-software.sh

--- remove-default-software.sh
+++ remove-default-software.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get -y remove --purge wolfram-engine penguinspuzzle scratch dillo squeak-vm squeak-plugins-scratch sonic-pi idle idle3 netsurf-gtk netsurf-common minecraft-pi nodered geany bluej greenfoot libreoffice* claws-mail
 
-sudo apt-get autoremove
-sudo apt-get clean
+sudo apt-get -y autoremove
+sudo apt-get -y clean
 
-rm -rf /home/pi/python_games
+sudo rm -rf /home/pi/python_games
